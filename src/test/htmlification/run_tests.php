4869c1680aec00a541cac98d3ef8731537bb6053
Josh Heidenreich <josh.sickmate@gmail.com>
2008-11-09 22:21:42 +0000

    Added a tool for testing the HTMLification system. HTMLification is the process of converting raw text into HTML that is meaningful. Version 0.2 of docu is going to have much better HTMLification, and so this testing tool should aid the coding of this area.
    The tool requires two files to be put into the 'htmlificaiton' directory for each test. The .txt file is what the user would enter into a DocBlock comment or the like, and the .htm file is the 
    expected output after HTMLification.
    
    This commit also includes two tests (both of which currently fail).
    
    If end users would like to submit tests, that would be greatly appreciated.

---
+++ src/test/htmlification/run_tests.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Copyright 2008 Josh Heidenreich
+
+This file is part of docu.
+
+Docu is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Docu is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with docu.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
+<style>
+body {font-size: small;}
+.success {background-color: #1ba63b; padding: 5px; margin-bottom: 5px;}
+.failure {background-color: #f99c9c; padding: 5px; margin-bottom: 5px;}
+div pre {border: 1px #777 solid;}
+</style>
+
+
+<?php
+/**
+* @package Testsuite
+**/
+
+
+require_once '../../processor/functions.php';
+
+$tests = glob('*.txt');
+
+foreach ($tests as $test_filename) {
+  $result_filename = str_replace ('.txt', '.htm', $test_filename);
+  
+  $test = file_get_contents($test_filename);
+  $expected_result = file_get_contents($result_filename);
+  
+  $real_result = htmlify_text($test);
+  
+  $expected_result = trim($expected_result);
+  $real_result = trim($real_result);
+  
+  if ($real_result == $expected_result) {
+    echo "<div class=\"success\">Test '{$test_filename}' was successful.</div>";
+    $success++;
+    
+  } else {
+    $real_result = htmlspecialchars ($real_result);
+    $expected_result = htmlspecialchars ($expected_result);
+    
+    echo "<div class=\"failure\">Test '{$test_filename}' failed.<br>";
+    echo "Result:<pre>{$real_result}</pre>";
+    echo "Expected:<pre>{$expected_result}</pre>";
+    echo '</div>';
+    $failure++;
+  }
+}
+
+echo "<p>{$success} test(s) were successful, {$failure} test(s) failed.</p>";
+?>
