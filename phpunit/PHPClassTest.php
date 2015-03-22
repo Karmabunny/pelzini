bb4da45f9e28a5c847a7bc574a31ac9b7b990748
Josh Heidenreich <josh.sickmate@gmail.com>
2015-03-23 08:44:24 +1030

    Added PHP tests for classes

--- phpunit/PHPClassTest.php
+++ phpunit/PHPClassTest.php
@@ -34,9 +34,152 @@ class PHPClassTest extends PHPUnit_Framework_TestCase {
         ');
         $this->assertCount(1, $file->classes);
         $this->assertEquals('aaa', $file->classes[0]->name);
+        $this->assertEquals(false, $file->classes[0]->abstract);
+        $this->assertCount(0, $file->classes[0]->functions);
         $this->assertCount(0, $file->functions);
         $this->assertCount(0, $file->constants);
         $this->assertCount(0, $file->enumerations);
+    }
+
+
+    /**
+    * Abstract classes
+    **/
+    public function testAbstract() {
+        $file = $this->parse('
+            <?php
+            abstract class aaa {}
+        ');
+        $this->assertCount(1, $file->classes);
+        $this->assertEquals('aaa', $file->classes[0]->name);
+        $this->assertEquals(true, $file->classes[0]->abstract);
+        $this->assertCount(0, $file->classes[0]->functions);
+    }
+
+
+    /**
+    * Classes with a function
+    **/
+    public function testFunction() {
+        $file = $this->parse('
+            <?php
+            class aaa {
+                function bbb() {}
+            }
+        ');
+        $this->assertCount(1, $file->classes);
+        $this->assertEquals('aaa', $file->classes[0]->name);
+        $this->assertEquals(false, $file->classes[0]->abstract);
+        $this->assertCount(1, $file->classes[0]->functions);
+        $this->assertEquals('bbb', $file->classes[0]->functions[0]->name);
+        $this->assertEquals(false, $file->classes[0]->functions[0]->abstract);
+        $this->assertEquals(false, $file->classes[0]->functions[0]->static);
+        $this->assertEquals('public', $file->classes[0]->functions[0]->visibility);
+    }
+
+
+    /**
+    * Classes with an abstract function
+    **/
+    public function testAbstractFunction() {
+        $file = $this->parse('
+            <?php
+            class aaa {
+                abstract function bbb() {}
+            }
+        ');
+        $this->assertCount(1, $file->classes);
+        $this->assertEquals('aaa', $file->classes[0]->name);
+        $this->assertEquals(false, $file->classes[0]->abstract);
+        $this->assertCount(1, $file->classes[0]->functions);
+        $this->assertEquals('bbb', $file->classes[0]->functions[0]->name);
+        $this->assertEquals(true, $file->classes[0]->functions[0]->abstract);
+        $this->assertEquals(false, $file->classes[0]->functions[0]->static);
+        $this->assertEquals('public', $file->classes[0]->functions[0]->visibility);
+    }
+
+
+    /**
+    * Classes with a static function
+    **/
+    public function testStaticFunction() {
+        $file = $this->parse('
+            <?php
+            class aaa {
+                static function bbb() {}
+            }
+        ');
+        $this->assertCount(1, $file->classes);
+        $this->assertEquals('aaa', $file->classes[0]->name);
+        $this->assertEquals(false, $file->classes[0]->abstract);
+        $this->assertCount(1, $file->classes[0]->functions);
+        $this->assertEquals('bbb', $file->classes[0]->functions[0]->name);
+        $this->assertEquals(false, $file->classes[0]->functions[0]->abstract);
+        $this->assertEquals(true, $file->classes[0]->functions[0]->static);
+        $this->assertEquals('public', $file->classes[0]->functions[0]->visibility);
+    }
+
+
+    /**
+    * Classes with a private function
+    **/
+    public function testPrivateFunction() {
+        $file = $this->parse('
+            <?php
+            class aaa {
+                private function bbb() {}
+            }
+        ');
+        $this->assertCount(1, $file->classes);
+        $this->assertEquals('aaa', $file->classes[0]->name);
+        $this->assertEquals(false, $file->classes[0]->abstract);
+        $this->assertCount(1, $file->classes[0]->functions);
+        $this->assertEquals('bbb', $file->classes[0]->functions[0]->name);
+        $this->assertEquals(false, $file->classes[0]->functions[0]->abstract);
+        $this->assertEquals(false, $file->classes[0]->functions[0]->static);
+        $this->assertEquals('private', $file->classes[0]->functions[0]->visibility);
+    }
+
+
+    /**
+    * Classes with a protected function
+    **/
+    public function testProtectedFunction() {
+        $file = $this->parse('
+            <?php
+            class aaa {
+                protected function bbb() {}
+            }
+        ');
+        $this->assertCount(1, $file->classes);
+        $this->assertEquals('aaa', $file->classes[0]->name);
+        $this->assertEquals(false, $file->classes[0]->abstract);
+        $this->assertCount(1, $file->classes[0]->functions);
+        $this->assertEquals('bbb', $file->classes[0]->functions[0]->name);
+        $this->assertEquals(false, $file->classes[0]->functions[0]->abstract);
+        $this->assertEquals(false, $file->classes[0]->functions[0]->static);
+        $this->assertEquals('protected', $file->classes[0]->functions[0]->visibility);
+    }
+
+
+    /**
+    * Classes with a (explicit) public function
+    **/
+    public function testPublicFunction() {
+        $file = $this->parse('
+            <?php
+            class aaa {
+                public function bbb() {}
+            }
+        ');
+        $this->assertCount(1, $file->classes);
+        $this->assertEquals('aaa', $file->classes[0]->name);
+        $this->assertEquals(false, $file->classes[0]->abstract);
+        $this->assertCount(1, $file->classes[0]->functions);
+        $this->assertEquals('bbb', $file->classes[0]->functions[0]->name);
+        $this->assertEquals(false, $file->classes[0]->functions[0]->abstract);
+        $this->assertEquals(false, $file->classes[0]->functions[0]->static);
+        $this->assertEquals('public', $file->classes[0]->functions[0]->visibility);
     }
 }
 
